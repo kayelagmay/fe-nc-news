f017be8e23e4bb45b4ffc53f58bcc04802e0f627
Kaye Lagmay <kayelagmay@icloud.com>
2024-12-18 14:46:43 +0000

    5 - View individual articles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import ArticleById from './ArticleById';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path="/articles/:article_id" element={<ArticleById />} />
       </Routes>
     </div>
   );
--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { get } from './utils/api';
+import { Link } from "react-router-dom";
 import './index.css';
 
 function ArticleCard() {
     const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         get('/articles')
-        .then((data) => setArticles(data.articles))
-        .catch((err) => console.error('Error fetching articles:', err));
+        .then((data) => {
+            setArticles(data.articles);
+            setIsLoading(false);
+        })
+        .catch((err) => {
+            setError(err.message);
+            setIsLoading(false);
+        });
     }, []);
+
+    if (isLoading) return <p>Loading articles...</p>;
+    if (error) return <p>Error: {error}</p>;
 
     return (
         <ul className='articleList'>
             {articles.map((article) => (
                 <li className='articleCard' key={article.article_id}>
-                <h3 className='articleTitle'>{article.title}</h3>
-                <img className='articleImg' src={article.article_img_url} alt='Article Image'/>
+                    <Link to={`/articles/${article.article_id}`}>
+                        <h3 className='articleTitle'>{article.title}</h3>
+                        <img className='articleImg' src={article.article_img_url} alt={article.title}/>
+                    </Link>
                 </li>
             ))}
         </ul>
